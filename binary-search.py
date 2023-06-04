af2a0bb0e7697e93bc737974e8f496ecfca22411
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-06-04 14:46:38 +0100

    Simple binary search update

--- binary-search.py
+++ binary-search.py
@@ -1,1 +1,20 @@
-# git test
+# Find the item in an array using binary search
+
+def binary_search(list: list, item) -> int:
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+list = [1, 3, 5, 7, 9]
+item = int(input("Please enter the item to search for: "))
+print(list[binary_search(list, item)])
